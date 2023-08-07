2c0a6c1535abe1a5d06a7603dc35f05c2d01ab1f
pepegateststuff <121232813+pepegateststuff@users.noreply.github.com>
2023-08-07 14:07:51 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,2 +1,9 @@
-local url = string.format('https://raw.githubusercontent.com/pepegateststuff/whitelist/main/Games/%s.lua',game.PlaceId)
-loadstring(game:HttpGet(url))()
+local gameLoader = string.format('https://raw.githubusercontent.com/pepegateststuff/whitelist/main/Games/%s.lua',game.PlaceId)
+local gameLink = "https://pepegateststuff.000webhostapp.com/check_game.php?id=" .. tostring(game.PlaceId)
+
+local request = syn and syn.request or request
+local response = request({Url = gameLink; Method = "GET"})
+
+if response["Body"] then
+	xpcall(loadstring(game:HttpGet(gameLoader)), print)
+end
