36b51a3818b25fa7c2ad82c5a0fb8e28e008be56
pepegateststuff <121232813+pepegateststuff@users.noreply.github.com>
2023-08-13 20:00:51 +0300

    Update 5529195348.lua

--- Games/5529195348.lua
+++ Games/5529195348.lua
@@ -5,7 +5,7 @@ local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))
 local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
 
 local Window = Library:CreateWindow({
-    Title = 'Rogue Spells',
+    Title = 'Kold Hub',
     Center = true, 
     AutoShow = true,
 })
