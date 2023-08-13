8f7f980c9d2365eed9385c4223558c2b90622c9c
pepegateststuff <121232813+pepegateststuff@users.noreply.github.com>
2023-08-13 20:07:44 +0300

    Update hwid.lua

--- hwid.lua
+++ hwid.lua
@@ -1,1 +1,1 @@
-local console_print = rconsoleprint or print; console_print(\"invalid hwid contact pepega\n\");
+local console_print = rconsoleprint or print; console_print("invalid hwid contact pepega\n");
