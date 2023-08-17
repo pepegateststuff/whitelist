20fedf86f4aa40bc6d0dce9cafa7f2574375c823
pepegateststuff <121232813+pepegateststuff@users.noreply.github.com>
2023-08-17 12:43:10 +0300

    Update 5529195348.lua

--- Games/5529195348.lua
+++ Games/5529195348.lua
@@ -5,7 +5,7 @@ local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))
 local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
 
 local Window = Library:CreateWindow({
-    Title = 'Kold Hub',
+    Title = 'kold',
     Center = true, 
     AutoShow = true,
 })
